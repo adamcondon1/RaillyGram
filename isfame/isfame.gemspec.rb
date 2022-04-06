416cf2817782ddee9199c06e90ba3ab84915d817
Adam Condon <adam.condon@phcol.ie>
2022-04-06 14:10:33 +0100

    tt

---
+++ isfame/isfame.gemspec.rb
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+ s.name = 'isfame'
+ s.version = '0.0.0'
+ s.date = '2022-04-03'
+ s.summary = "Test to see if a following is famous"
+ s.description = "A simple gem"
+ s.authors = ["AdamC"]
+ s.email = 'ac@email.ie'
+ s.homepage = 'http://rubygems.org/gems/isfame'
+ s.files = ["lib/isfame.rb"]
+end
---
+++ isfame/lib/isfame.rb
@@ -0,0 +1,14 @@
+class Checkfame
+ def self.runcheck(isFame)
+ if isFame < 0
+ ans = "Not Valid"
+elsif (isFame > 0 && (isFame < 1000))
+ ans = "Not Famous"
+ else
+ if (isFame > 1000)
+ ans = "Your Famous"
+ else
+ end
+ end
+ end
+end
